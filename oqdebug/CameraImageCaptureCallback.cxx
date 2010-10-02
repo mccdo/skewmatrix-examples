fb85019f6bc4c4617b9239898bc7912ea2617a70
Paul Martz <pmartz@skew-matrix.com>
2010-10-02 17:10:02 +0000

    Reproducer case for OQ crash.

---
+++ oqdebug/CameraImageCaptureCallback.cxx
@@ -0,0 +1,84 @@
+/*************** <auto-copyright.rb BEGIN do not edit this line> **************
+ *
+ * VE-Suite is (C) Copyright 1998-2010 by Iowa State University
+ *
+ * Original Development Team:
+ *   - ISU's Thermal Systems Virtual Engineering Group,
+ *     Headed by Kenneth Mark Bryden, Ph.D., www.vrac.iastate.edu/~kmbryden
+ *   - Reaction Engineering International, www.reaction-eng.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * Date modified: $Date: 2010-09-29 20:49:01 -0500 (Wed, 29 Sep 2010) $
+ * Version:       $Rev: 14907 $
+ * Author:        $Author: mccdo $
+ * Id:            $Id: CameraImageCaptureCallback.cxx 14907 2010-09-30 01:49:01Z mccdo $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.rb END do not edit this line> ***************/
+
+#include "CameraImageCaptureCallback.h"
+
+// --- OSG Includes --- //
+#include <osg/Image>
+#include <osg/io_utils>
+#include <osg/Texture2D>
+
+#include <osgDB/WriteFile>
+#include <osgDB/FileUtils>
+
+#include <iostream>
+
+
+////////////////////////////////////////////////////////////////////////////////
+CameraImageCaptureCallback::CameraImageCaptureCallback(
+    const std::string& filename, int w, int h, osg::Texture2D* tex )
+    :
+    m_filename( filename ),
+    width( w ),
+    height( h ),
+    m_tex( tex )
+{
+    ;
+}
+////////////////////////////////////////////////////////////////////////////////
+void CameraImageCaptureCallback::operator()( osg::RenderInfo& ri ) const
+{
+    //std::cout << ri.getCurrentCamera() << std::endl;
+    //m_tex->copyTexImage2D( *ri.getState(), 0, 0, 1024, 1024 );
+
+    osg::Image* image = m_tex->getImage();
+    std::string fName( m_filename );
+
+    osg::notify( osg::ALWAYS ) << "Reading image for file " 
+        << fName << " ... " << std::endl;
+    //const osg::Viewport* vp = ri.getState()->getCurrentViewport();    
+    //image->readPixels( 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE );
+
+    //
+    // Get the texture image.
+    m_tex->apply( *( ri.getState() ) );
+    image->readImageFromCurrentTexture( ri.getContextID(), false, GL_UNSIGNED_BYTE );
+    // Got it.
+    //
+
+    osg::notify( osg::ALWAYS ) << "Writing file " 
+        << fName << " ... " << std::endl;
+    osgDB::writeImageFile( *image, fName );
+    osg::notify( osg::ALWAYS ) << "Capture complete." << std::endl;
+}
+////////////////////////////////////////////////////////////////////////////////
---
+++ oqdebug/pqdebug.cpp
@@ -0,0 +1,166 @@
+// Copyright (c) 2008 Skew Matrix Software LLC. All rights reserved.
+
+#include <osg/Node>
+#include <osg/Camera>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/Texture2D>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osgwTools/Shapes.h>
+#include <osgwTools/Version.h>
+#include <osg/OcclusionQueryNode>
+
+#define USE_ISU_CB
+#if defined( USE_ISU_CB )
+#include "CameraImageCaptureCallback.h"
+#else
+#include <osgwTools/ScreenCapture.h>
+#endif
+
+#include <string>
+
+
+//
+// Begin globalc
+const int winW( 800 ), winH( 600 );
+
+osg::ref_ptr< osg::Texture2D > tex;
+
+osg::ref_ptr< osg::Camera > preRenderCamera;
+
+#if defined( USE_ISU_CB )
+std::string imageDumpName( "out.png" );
+osg::ref_ptr< CameraImageCaptureCallback > captureCB;
+#else
+osg::ref_ptr< osgwTools::ScreenCapture > captureCB;
+#endif
+
+// End globals
+//
+
+
+
+class Handler : public osgGA::GUIEventHandler 
+{
+public: 
+    Handler() : _frameCount( -1 ) {}
+    ~Handler() {}
+
+    bool handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa )
+    {
+        bool handled( false );
+        switch(ea.getEventType())
+        {
+            case( osgGA::GUIEventAdapter::FRAME ):
+            {
+#if defined( USE_ISU_CB )
+                if( _frameCount > -1 )
+                {
+                    if( --_frameCount == 0 )
+                        preRenderCamera->setPostDrawCallback( NULL );
+                }
+#endif
+            }
+            case( osgGA::GUIEventAdapter::KEYDOWN ):
+            {
+                switch( ea.getKey() )
+                {
+                    case( osgGA::GUIEventAdapter::KEY_Return ):
+                    {
+#if defined( USE_ISU_CB )
+                        preRenderCamera->setPostDrawCallback( captureCB.get() );
+                        _frameCount = 2;
+#else
+                        captureCB->setNumFramesToCapture( 1 );
+                        captureCB->setCapture( true );
+#endif
+                        handled = true;
+                    }
+                }
+            }
+        }
+        return( handled );
+    }
+
+    int _frameCount;
+};
+
+
+osg::Node*
+preRender( osg::Group* root, osg::Node* model )
+{
+    // Create the texture; we'll use this as our color buffer.
+    // Note it has no image data; not required.
+    tex = new osg::Texture2D;
+    tex->setTextureWidth( winW );
+    tex->setTextureHeight( winH );
+    tex->setInternalFormat( GL_RGBA );
+    tex->setBorderWidth( 0 );
+    tex->setFilter( osg::Texture::MIN_FILTER, osg::Texture::NEAREST );
+    tex->setFilter( osg::Texture::MAG_FILTER, osg::Texture::NEAREST );
+#if defined( USE_ISU_CB )
+    osg::Image* image = new osg::Image;
+    tex->setImage( image );
+#endif
+
+
+    // Configure preRenderCamera to draw fullscreen textured quad
+    preRenderCamera = new osg::Camera;
+    preRenderCamera->setClearColor( osg::Vec4( .5, 0., 0., 1. ) );
+
+    preRenderCamera->setReferenceFrame( osg::Camera::ABSOLUTE_RF );
+    preRenderCamera->setRenderOrder( osg::Camera::PRE_RENDER );
+    preRenderCamera->setViewMatrix( osg::Matrixd::translate( 0., 0., -30. ) );
+    preRenderCamera->setProjectionMatrix( osg::Matrixd::perspective( 25., 1., 1., 100. ) );
+
+    preRenderCamera->setRenderTargetImplementation( osg::Camera::FRAME_BUFFER_OBJECT, osg::Camera::FRAME_BUFFER );
+    preRenderCamera->attach( osg::Camera::COLOR_BUFFER0, tex.get(), 0, 0, false );
+
+    osg::OcclusionQueryNode* oqn = new osg::OcclusionQueryNode;
+    oqn->addChild( model );
+    preRenderCamera->addChild( oqn );
+
+#if defined( USE_ISU_CB )
+    captureCB = new CameraImageCaptureCallback( imageDumpName, 512, 512, tex );
+#else
+    captureCB = new osgwTools::ScreenCapture;
+    preRenderCamera->setPostDrawCallback( captureCB.get() );
+#endif
+
+
+    // Configure textured tri pair to hang off the root node and
+    // display our texture.
+    osg::Geode* geode( new osg::Geode );
+    geode->addDrawable( osgwTools::makePlane(
+        osg::Vec3( -1,0,-1 ), osg::Vec3( 2,0,0 ), osg::Vec3( 0,0,2 ) ) );
+    geode->getOrCreateStateSet()->setTextureAttributeAndModes(
+        0, tex, osg::StateAttribute::ON );
+    geode->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
+
+    root->addChild( geode );
+
+
+    return( preRenderCamera.get() );
+}
+
+int
+main( int argc, char** argv )
+{
+    osg::ref_ptr< osg::Group > root( new osg::Group );
+    osg::ref_ptr< osg::Node > model( osgDB::readNodeFile( "cow.osg" ) );
+    if( !model.valid() )
+        return( 1 );
+
+    osgViewer::Viewer viewer;
+    viewer.setUpViewInWindow( 10, 30, winW, winH );
+    viewer.setSceneData( root.get() );
+    viewer.realize();
+    viewer.addEventHandler( new Handler );
+
+    root->addChild( preRender( root.get(), model.get() ) );
+
+    viewer.getCamera()->setClearColor( osg::Vec4( 0.4, 0.4, 0.4, 1. ) );
+
+    return( viewer.run() );
+}
